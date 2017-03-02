2def6c4e4f0db20f5355877dc2d728bf406891e6
ramikhashmel <rm808585@dal.ca>
2017-03-02 14:45:52 -0400

    email-validator

--- Validator.java
+++ Validator.java
@@ -3,6 +3,16 @@ package com.emailui.email_validator_ui;
 public class Validator{
 	public Validator(){}
 	
-	
+	public boolean validate(String email){
+		int rule = false;
+		//checks if for '.' char
+		if (email.contains(".")){
+			rule = true;
+		}
+		//checks for a single @char
+		if (email.matches("^[^@]*@[^@]*$")){
+			rule++;
+		}
+		return rule;
 	}
 }
